927ed30e4cb209562680914f67a474ac4da5e674
Lobo Metalúrgico <43734867+LoboMetalurgico@users.noreply.github.com>
2024-08-25 21:59:17 -0300

    (feat): full reboot

---
+++ WhatsPendingApp/ViewModels/MainViewModel.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace WhatsPendingApp.ViewModels;
+
+public partial class MainViewModel : ViewModelBase
+{
+    [ObservableProperty] private string _greeting = "Come hang out";
+}
---
+++ WhatsPendingApp/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace WhatsPendingApp.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
